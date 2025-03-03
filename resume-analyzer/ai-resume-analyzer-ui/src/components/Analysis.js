a9fb25f9f3b88785ba8fffba9763469251caca74
Paul Morales <paulmorales@pauls-mbp.mynetworksettings.com>
2025-03-03 13:45:55 -0500

    created analysis component to display the results of the AI

---
+++ resume-analyzer/ai-resume-analyzer-ui/src/components/Analysis.js
@@ -0,0 +1,26 @@
+import React from "react";
+import styles from './Analysis.module.css';
+
+const Analysis = ({ results }) => {
+    return(
+        <div className={styles.resultsContainer}>
+      <h3>Analysis Results</h3>
+      <p><strong>Compatibility Score:</strong> {results.score}%</p>
+      <h4>Suggested Keywords:</h4>
+      {results?.suggestedKeywords?.length > 0 ? (
+        <ul>
+          {results.suggestedKeywords.map((keyword, index) => (
+            <li key={index}>{keyword}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>No suggested keywords.</p>
+      )}
+      <h4>Feedback:</h4>
+      <p>{results.feedback}</p>
+    </div>
+
+    )
+}
+
+export default Analysis;
